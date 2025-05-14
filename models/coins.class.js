b18401271e8b46f3b5629ab97537b569d78622a3
Joshua Plischek <plischekjoshua@web.de>
2025-05-14 18:09:11 +0200

    Add coin class

---
+++ models/coins.class.js
@@ -0,0 +1,13 @@
+class Coins extends CollectableObjects {
+
+    IMAGES_COINS = [
+        'img/4. Marcadores/1. Coins/1.png',
+        'img/4. Marcadores/1. Coins/2.png',
+        'img/4. Marcadores/1. Coins/3.png',
+        'img/4. Marcadores/1. Coins/4.png',
+    ];
+
+    constructor() {
+        super();
+    }
+}
